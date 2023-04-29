711e09e8ed04d76a65ca5165b306d1005f9bfad4
zhongminhu <zhongminhu@tencent.com>
2023-04-29 11:47:53 +0800

    Made a simple layout.

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,97 +1,45 @@
-<script setup lang="ts">
-import Versions from './components/Versions.vue'
-</script>
-
 <template>
-  <Versions></Versions>
-
-  <svg class="hero-logo" viewBox="0 0 900 300">
-    <use xlink:href="./assets/icons.svg#electron" />
-  </svg>
-  <h2 class="hero-text">You've successfully created an Electron project with Vue and TypeScript</h2>
-  <p class="hero-tagline">Please try pressing <code>F12</code> to open the devTool</p>
-
-  <div class="links">
-    <div class="link-item">
-      <a target="_blank" href="https://evite.netlify.app">Documentation</a>
-    </div>
-    <div class="link-item link-dot">•</div>
-    <div class="link-item">
-      <a target="_blank" href="https://github.com/alex8088/electron-vite">Getting Help</a>
-    </div>
-    <div class="link-item link-dot">•</div>
-    <div class="link-item">
-      <a
-        target="_blank"
-        href="https://github.com/alex8088/quick-start/tree/master/packages/create-electron"
-      >
-        create-electron
-      </a>
-    </div>
-  </div>
-
-  <div class="features">
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Configuring</h2>
-        <p class="detail">
-          Config with <span>electron.vite.config.ts</span> and refer to the
-          <a target="_blank" href="https://evite.netlify.app/config/">config guide</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">HMR</h2>
-        <p class="detail">
-          Edit <span>src/renderer</span> files to test HMR. See
-          <a target="_blank" href="https://evite.netlify.app/guide/hmr-in-renderer.html">docs</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Hot Reloading</h2>
-        <p class="detail">
-          Run <span>'electron-vite dev --watch'</span> to enable. See
-          <a target="_blank" href="https://evite.netlify.app/guide/hot-reloading.html">docs</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Debugging</h2>
-        <p class="detail">
-          Check out <span>.vscode/launch.json</span>. See
-          <a target="_blank" href="https://evite.netlify.app/guide/debugging.html">docs</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Source Code Protection</h2>
-        <p class="detail">
-          Supported via built-in plugin <span>bytecodePlugin</span>. See
-          <a target="_blank" href="https://evite.netlify.app/guide/source-code-protection.html">
-            docs
-          </a>
-          .
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Packaging</h2>
-        <p class="detail">
-          Use
-          <a target="_blank" href="https://www.electron.build">electron-builder</a>
-          and pre-configured to pack your app.
-        </p>
-      </article>
-    </div>
-  </div>
+  <a-layout>
+    <a-layout-sider>
+      <div class="menu-demo">
+        <a-menu
+          :style="{ width: '200px', height: '100%' }"
+          :default-open-keys="['0']"
+          :default-selected-keys="['0_1']"
+          show-collapse-button
+        >
+          <a-menu-item key="0_0_0" data-obj="1">ArtBox</a-menu-item>
+          <a-sub-menu key="0">
+            <template #icon><icon-apps></icon-apps></template>
+            <template #title>Quantum Keys</template>
+            <a-menu-item key="0_0">PublicKey Generation</a-menu-item>
+            <a-menu-item key="0_1">Digital Signature</a-menu-item>
+            <a-menu-item key="0_2">Verify Signature</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="1">
+            <template #icon><icon-bug></icon-bug></template>
+            <template #title>AIGC</template>
+            <a-menu-item key="1_0">StableDiffusion</a-menu-item>
+            <a-menu-item key="1_1">MidJourney</a-menu-item>
+            <a-menu-item key="1_2">Dall-E</a-menu-item>
+            <a-menu-item key="1_3">InfMonkeys</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="3">Settings</a-menu-item>
+        </a-menu>
+      </div>
+    </a-layout-sider>
+    <a-layout-content>Content</a-layout-content>
+  </a-layout>
 </template>
 
-<style lang="less">
-@import './assets/css/styles.less';
+<script lang="ts" setup></script>
+
+<style scoped>
+.menu-demo {
+  box-sizing: border-box;
+  width: 100%;
+  height: 600px;
+  /* padding: 40px; */
+  background-color: var(--color-neutral-2);
+}
 </style>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 
-createApp(App).mount('#app')
+createApp(App).use(ArcoVue).use(ArcoVueIcon).mount('#app')
